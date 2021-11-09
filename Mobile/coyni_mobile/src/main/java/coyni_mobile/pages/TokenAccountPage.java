a045feb73ed70fcd4ef03020495d3c3562d3f2c9
ideyaLabs <ideyaLabs@host.docker.internal>
2021-11-09 16:56:39 +0530

    Token Account

---
+++ Mobile/coyni_mobile/src/main/java/coyni_mobile/pages/TokenAccountPage.java
@@ -0,0 +1,11 @@
+package coyni_mobile.pages;
+
+import org.openqa.selenium.By;
+
+import io.appium.java_client.MobileBy;
+
+public class TokenAccountPage {
+
+	private By lblAccountHolderName = MobileBy.xpath("");
+
+}
