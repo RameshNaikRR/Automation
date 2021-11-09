e3661799a99d9f713953308ed4fe0072fba754a1
ideyaLabs <ideyaLabs@host.docker.internal>
2021-11-09 16:39:01 +0530

    Login Page

---
+++ Mobile/coyni_mobile/src/main/java/coyni_mobile/pages/ForgotPasswordPage.java
@@ -0,0 +1,61 @@
+package coyni_mobile.pages;
+
+
+
+import org.openqa.selenium.By;
+
+
+
+import coyni_mobile.utilities.CommonFunctions;
+import ilabs.MobileFramework.MobileFunctions;
+import io.appium.java_client.MobileBy;
+
+
+
+public class ForgotPasswordPage extends MobileFunctions {
+
+
+
+private By lnkForgotPassword = MobileBy.xpath("");
+private By heading = MobileBy.xpath("");
+private By txtEmail = MobileBy.xpath("");
+private By btnClose = MobileBy.xpath("");
+private By btnNext = MobileBy.xpath("");
+private By lblEmail = MobileBy.xpath("");
+private By lnkResend = MobileBy.xpath("");
+private By txtInputBoxes = MobileBy.xpath("");
+
+public void clickForgotPassword() {
+click(lnkForgotPassword, "Forgot Password");
+}
+
+public void verifyHeading(String expHeading) {
+//new CommonFunctions().verifyLabelText(heading, "Forgot Password ", expHeading);
+}
+
+public void fillEmail(String email) {
+enterText(txtEmail, email, "Email");
+}
+
+public void clickClose() {
+click(btnClose, "Close");
+}
+
+public void clickNext() {
+click(btnNext, "Next");
+}
+
+
+
+public void verifyPhoneNumber(String expEmail) {
+//new CommonFunctions().verifyLabelText(lblEmail, "exp Email", expEmail);
+}
+
+public void clickResend() {
+click(lnkResend, "Resend");
+}
+
+public void fillInputBoxes(String otp) {
+enterText(txtInputBoxes, otp, "OTP");
+}
+}
--- Mobile/coyni_mobile/src/main/java/coyni_mobile/pages/LoginPage.java
+++ Mobile/coyni_mobile/src/main/java/coyni_mobile/pages/LoginPage.java
@@ -1,37 +1,54 @@
 package coyni_mobile.pages;
 
+
+
 import org.openqa.selenium.By;
+
+
 
 import ilabs.MobileFramework.MobileFunctions;
 import io.appium.java_client.MobileBy;
 
+
+
 public class LoginPage extends MobileFunctions {
-    private By txtUserName = MobileBy.id("com.coyni.android:id/etEmail");
-    private By txtPassword = MobileBy.id("com.coyni.android:id/etPassword");
-    private By btnLogin = MobileBy.id("com.coyni.android:id/cvLogin");
-    private By lnkSignUp = MobileBy.linkText("Sign up");
-    private By lnkForgotPassword = MobileBy.linkText("com.coyni.android:id/tvForgot");
+private By txtemail = MobileBy.xpath("");
+private By txtPassword = MobileBy.xpath("");
+private By btnRemenberMe = MobileBy.xpath("");
+private By btnLogin = MobileBy.xpath("");
 
-    public void fillUserName(String userName){
-        enterText(txtUserName, userName, "username");
-    }
 
-    public void fillPassword(String password){
-        enterText(txtPassword, password, "password");
-    }
 
-    public void clickLogin(){
-        click(btnLogin, "login button");
-    }
+public void fillUserName(String email) {
+enterText(txtemail, email, "Email");
+}
 
-    public CreateAnAccountPage clickSingupLink(){
-        click(lnkSignUp, "sign up link");
-        return new CreateAnAccountPage();
-    }
 
-    public void clickForgotPasswordLink(){
-        click(lnkForgotPassword, "forgot password link");
-    }
+
+public void fillPassword(String password) {
+enterText(txtPassword, password, "password");
+}
+
+
+
+public void clickRemenberMe() {
+click(btnRemenberMe, "RemenberMe");
+}
+
+
+
+public void clickLogin() {
+click(btnLogin, "login button");
+}
+
+
+
+public ForgotPasswordPage forgotPasswordPage() {
+return new ForgotPasswordPage();
+}
+
+
+
 
 
 }
