53358a9a0306afb067928f8046960ec5476aa951
ideyaLabs <ideyaLabs@DESKTOP-81IRURQ>
2021-11-11 11:56:53 +0530

    Added new methods in common Functions

--- Mobile/coyni_mobile/src/main/java/coyni_mobile/utilities/CommonFunctions.java
+++ Mobile/coyni_mobile/src/main/java/coyni_mobile/utilities/CommonFunctions.java
@@ -1,7 +1,10 @@
 package coyni_mobile.utilities;
 
+import org.openqa.selenium.By;
+
 import ilabs.MobileFramework.MobileFunctions;
 import ilabs.mobile.reporting.ExtentTestManager;
+import io.appium.java_client.MobileBy;
 
 public class    CommonFunctions {
 
@@ -11,11 +14,53 @@ public class    CommonFunctions {
         mobileFunctions = new MobileFunctions();
     }
 
-    public void verifyLabelText(String labelName, String expText, String actText){
-        if (expText.equalsIgnoreCase(actText)){
-            ExtentTestManager.setPassMessageInReport(String.format("%s is %s", labelName, actText));
-        } else {
-            ExtentTestManager.setFailMessageInReport(String.format("%s ::<p>Expected =  %s</br>Actual = %s</p>", labelName, expText, actText));
-        }
-    }
+//    public void verifyLabelText(String labelName, String expText, String actText){
+//        if (expText.equalsIgnoreCase(actText)){
+//            ExtentTestManager.setPassMessageInReport(String.format("%s is %s", labelName, actText));
+//        } else {
+//            ExtentTestManager.setFailMessageInReport(String.format("%s ::<p>Expected =  %s</br>Actual = %s</p>", labelName, expText, actText));
+//        }
+//    }
+
+
+public void verifyLabelText(By label, String labelName, String expText){
+	String actText = mobileFunctions.getText(label).trim().replace("\n", "").replace(",", "");
+	if (expText.equalsIgnoreCase(actText)){
+		ExtentTestManager.setPassMessageInReport(String.format("%s is %s", labelName, actText));
+	} else {
+		ExtentTestManager.setFailMessageInReport(String.format("%s ::<p>Expected =  %s</br>Actual = %s</p>", labelName, expText, actText));
+	}
+}
+
+public void elementView(By ele, String eleName) {
+	try {
+		if(mobileFunctions.getElement(ele, eleName).isDisplayed()) {
+			ExtentTestManager.setPassMessageInReport(eleName + " is displayed ");
+		}
+		else {
+			ExtentTestManager.setFailMessageInReport(eleName + " is not displayed ");
+		}
+	}
+	catch(Exception e) {
+		ExtentTestManager.setFailMessageInReport(" element View failed due to exception " + e);
+	}
+	
+}
+
+public void validateFormErrorMessage(String expErrMsg) {
+	try {
+		By errorMsgs = MobileBy.xpath("");
+		mobileFunctions.waitForVisibility(errorMsgs);
+		boolean status = mobileFunctions.getElementList(errorMsgs,"error Message").stream()
+				.map(ele -> ele.getText().toLowerCase()).anyMatch(msg -> msg.contains(expErrMsg.toLowerCase()));
+		if (status) {
+			ExtentTestManager.setPassMessageInReport("Error message '" + expErrMsg + "' displayed");
+		} else {
+			ExtentTestManager.setFailMessageInReport("Error message '" + expErrMsg + "' not displayed");
+		}
+	} catch (Exception e) {
+		ExtentTestManager.setFailMessageInReport("validate form error message failed due to exception " + e);
+	}
+}
+
 }
