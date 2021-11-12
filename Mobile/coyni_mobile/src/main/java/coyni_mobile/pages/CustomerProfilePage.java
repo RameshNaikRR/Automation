bcb9c6880183efcaf9e1c12e4e449609801bc2fd
prajwalreddychennur <prajwalc@ideyalabs.com>
2021-11-12 17:25:09 +0530

    Added QR COde test methods in Customer Profile test

--- Mobile/coyni_mobile/src/main/java/coyni_mobile/pages/CustomerProfilePage.java
+++ Mobile/coyni_mobile/src/main/java/coyni_mobile/pages/CustomerProfilePage.java
@@ -2,71 +2,132 @@ package coyni_mobile.pages;
 
 import org.openqa.selenium.By;
 
+import coyni_mobile.components.NavigationComponent;
 import coyni_mobile.utilities.CommonFunctions;
 import ilabs.MobileFramework.MobileFunctions;
 import io.appium.java_client.MobileBy;
 
 public class CustomerProfilePage extends MobileFunctions {
-	private By lblUserName =MobileBy.xpath("");
-	private By lblAccountID =MobileBy.xpath("");
-    private By lblAccountStatus =MobileBy.xpath("");
-    
-    private By lnkUserDetails =MobileBy.xpath("");
-    private By lnkPaymentMethods =MobileBy.xpath("");
-    private By lnkPreferences =MobileBy.xpath("");
-    private By lnkAccountLimits =MobileBy.xpath("");
-    private By lnkAgreements=MobileBy.xpath("");
-    private By lnkResetPinCode =MobileBy.xpath("");
-    private By btnFaceIDSetting =MobileBy.xpath("");
-    private By lnkChangePassword =MobileBy.xpath("");
-    private By btnLogOut =MobileBy.xpath("");
-    
-    
-    public void verifyUserName(String expUserName) {
-    	new CommonFunctions().verifyLabelText(lblUserName, "User Name", expUserName);
-    }
-    public void verifyAccountID(String expAccountID) {
-    	new CommonFunctions().verifyLabelText(lblAccountID, "Account ID", expAccountID);
-    }
-   public void verifyAccountStatus(String expStatus) {
-	   new CommonFunctions().verifyLabelText(lblAccountStatus, "Account Status", expStatus);
-   }
-   public void clickUserDetails() {
-	   click(lnkUserDetails	, " Click User Details");
-   }
+	private By imgUser = MobileBy.xpath("");
+	private By lblUserName = MobileBy.xpath("");
+	private By lblAccountID = MobileBy.xpath("");
+	private By lblAccountStatus = MobileBy.xpath("");
 
-   public void clickPaymentMethods() {
-	   click(lnkPaymentMethods, "Click Payment Methods");
-   }
-   public void clickPreferences() {
-	   click(lnkPreferences, "Click Preferences");
-   }
-   public void clickAccountLimits() {
-	   click(lnkAccountLimits, "click Account limits");
-   }
-   public void clickAgreements() {
-	   click(lnkAgreements, "Click Agreements");
-   }
-   public void clickResetPinCode() {
-	   click(lnkResetPinCode, "Click Reset Pin Code");
-   }
-   public void clickFaceIDSetting() {
-	   click(btnFaceIDSetting, "Click FaceID Setting");
-   }
-   public void clickChangePassword() {
-	   click(lnkChangePassword, "Click Change Password");
-   }
-   public void clickLogOut() {
-	   click(btnLogOut, "Click LogOut");
-   }
-   public void verifyProfilePageView() {
-	   new CommonFunctions().elementView(lblUserName, "UserName");
-   }
-   
-   public void verifyUserDetailsView() {
+	private By imgQRCode = MobileBy.xpath("");
+	private By lblMyQRCodeHeading = MobileBy.xpath("");
+	private By btnShare = MobileBy.xpath("");
+	private By popup = MobileBy.xpath("");
+	private By lblCopyMessage = MobileBy.xpath("");
+	private By lnkUserDetails = MobileBy.xpath("");
+	private By lnkPaymentMethods = MobileBy.xpath("");
+	private By lnkPreferences = MobileBy.xpath("");
+	private By lnkAccountLimits = MobileBy.xpath("");
+	private By lnkAgreements = MobileBy.xpath("");
+	private By lnkResetPinCode = MobileBy.xpath("");
+	private By btnFaceIDSetting = MobileBy.xpath("");
+	private By lnkChangePassword = MobileBy.xpath("");
+	private By btnLogOut = MobileBy.xpath("");
+
+	public void verifyUserName(String expUserName) {
+		new CommonFunctions().verifyLabelText(lblUserName, "User Name", expUserName);
+	}
+
+	public void verifyAccountID(String expAccountID) {
+		new CommonFunctions().verifyLabelText(lblAccountID, "Account ID", expAccountID);
+	}
+
+	public void verifyAccountStatus(String expStatus) {
+		new CommonFunctions().verifyLabelText(lblAccountStatus, "Account Status", expStatus);
+	}
+
+	public void clickUserDetails() {
+		click(lnkUserDetails, " Click User Details");
+	}
+
+	public void clickPaymentMethods() {
+		click(lnkPaymentMethods, "Click Payment Methods");
+	}
+
+	public void clickPreferences() {
+		click(lnkPreferences, "Click Preferences");
+	}
+
+	public void clickAccountLimits() {
+		click(lnkAccountLimits, "click Account limits");
+	}
+
+	public void clickAgreements() {
+		click(lnkAgreements, "Click Agreements");
+	}
+
+	public void clickResetPinCode() {
+		click(lnkResetPinCode, "Click Reset Pin Code");
+	}
+
+	public void clickFaceIDSetting() {
+		click(btnFaceIDSetting, "Click FaceID Setting");
+	}
+
+	public void clickChangePassword() {
+		click(lnkChangePassword, "Click Change Password");
+	}
+
+	public void clickLogOut() {
+		click(btnLogOut, "Click LogOut");
+	}
+
+	public void clickQRImage() {
+		click(imgQRCode, "Click QR Code ");
+
+	}
+
+	public void clickShare() {
+		click(btnShare, "Share ");
+	}
+
+	public void verifyProfilePageView() {
+		new CommonFunctions().elementView(lblUserName, "UserName");
+	}
+
+	public void verifyUserDetailsView() {
 		new CommonFunctions().elementView(lnkUserDetails, "User Details");
 	}
-public UserDetailsPage userDetailsPage() {
-	return new UserDetailsPage();
-}
+
+	public void verifyUserNameView() {
+		new CommonFunctions().elementView(lblUserName, "UserName");
+	}
+
+	public UserDetailsPage userDetailsPage() {
+		return new UserDetailsPage();
+	}
+
+	public void verifyLabelCopyText() {
+		new CommonFunctions().elementView(lblCopyMessage, "Copied Message");
+	}
+
+	public void verifyImageUserView() {
+		new CommonFunctions().elementView(imgUser, "Image User Initials ");
+	}
+
+	public void verifyAccountIDView() {
+		new CommonFunctions().elementView(lblAccountID, "Account ID ");
+	}
+
+	public void verifyAccountStatusView() {
+		new CommonFunctions().elementView(lblAccountStatus, "Account ID ");
+	}
+
+	public void verifyLabelMyQRCodeView() {
+		new CommonFunctions().elementView(lblMyQRCodeHeading, "Image QR ");
+
+	}
+
+	public void verifyPopUp() {
+		new CommonFunctions().elementView(popup, "Verify Popup");
+
+	}
+
+	public NavigationComponent navigationComponent() {
+		return new NavigationComponent();
+	}
 }
--- Mobile/coyni_mobile/src/test/java/coyni_mobile/tests/CustomerProfileTest.java
+++ Mobile/coyni_mobile/src/test/java/coyni_mobile/tests/CustomerProfileTest.java
@@ -6,7 +6,9 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import coyni_mobile.components.NavigationComponent;
 import coyni_mobile.pages.CustomerProfilePage;
+import coyni_mobile.pages.TokenAccountPage;
 import coyni_mobile.pages.UserDetailsPage;
 import ilabs.MobileFramework.Runner;
 import ilabs.mobile.reporting.ExtentTestManager;
@@ -15,33 +17,38 @@ public class CustomerProfileTest {
 
 	CustomerProfilePage customerProfilePage;
 	UserDetailsPage userDetailsPage;
-	
+	TokenAccountPage tokenAccountPage;
+	NavigationComponent navigationComponent;
+
 	@BeforeTest
 	public void init() {
-		customerProfilePage =new CustomerProfilePage();
-	    userDetailsPage =new UserDetailsPage();	
+		customerProfilePage = new CustomerProfilePage();
+		userDetailsPage = new UserDetailsPage();
+		tokenAccountPage = new TokenAccountPage();
+		navigationComponent = new NavigationComponent();
 	}
+
 	@Test
-	@Parameters({"strParams"})
+	@Parameters({ "strParams" })
 	public void testUserDetailsView(String strParams) {
 		try {
 			Map<String, String> data = Runner.getKeywordParameters(strParams);
 
-		customerProfilePage.verifyUserDetailsView();
-		customerProfilePage.clickUserDetails();
-		customerProfilePage.userDetailsPage().verifyHeading(data.get("heading"));
-		customerProfilePage.userDetailsPage().verifyEditUserImageView();
-		customerProfilePage.userDetailsPage().navigationComponent().verifyBackView();
-		customerProfilePage.userDetailsPage().navigationComponent().clickBack();
-		customerProfilePage.verifyProfilePageView();
-		
-		}catch(Exception e) {
-			ExtentTestManager.setFailMessageInReport("Test user details view failed due to exception " + e );
+			customerProfilePage.verifyUserDetailsView();
+			customerProfilePage.clickUserDetails();
+			customerProfilePage.userDetailsPage().verifyHeading(data.get("heading"));
+			customerProfilePage.userDetailsPage().verifyEditUserImageView();
+			customerProfilePage.userDetailsPage().navigationComponent().verifyBackView();
+			customerProfilePage.userDetailsPage().navigationComponent().clickBack();
+			customerProfilePage.verifyProfilePageView();
+
+		} catch (Exception e) {
+			ExtentTestManager.setFailMessageInReport("Test user details view failed due to exception " + e);
 		}
 	}
-	
+
 	@Test
-	@Parameters({"strParams"})
+	@Parameters({ "strParams" })
 	public void testEditImageView(String strParams) {
 		try {
 			Map<String, String> data = Runner.getKeywordParameters(strParams);
@@ -51,20 +58,94 @@ public class CustomerProfileTest {
 			customerProfilePage.userDetailsPage().editUserImagePopup().verifyTakeAPhotoView();
 			customerProfilePage.userDetailsPage().editUserImagePopup().clickChooseFromLibrary();
 			customerProfilePage.userDetailsPage().editUserImagePopup().chooseFromLibraryPopup().verifyLibraryView();
-			customerProfilePage.userDetailsPage().editUserImagePopup().chooseFromLibraryPopup().navigationComponent().verifyCloseView();
+			customerProfilePage.userDetailsPage().editUserImagePopup().chooseFromLibraryPopup().navigationComponent()
+					.verifyCloseView();
 			customerProfilePage.userDetailsPage().editUserImagePopup().chooseFromLibraryPopup().verifyDoneView();
 			customerProfilePage.userDetailsPage().editUserImagePopup().chooseFromLibraryPopup().clickImage();
 			customerProfilePage.userDetailsPage().editUserImagePopup().chooseFromLibraryPopup().clickDone();
 			customerProfilePage.userDetailsPage().verifyHeading(data.get("heading"));
-			//Navigation userDetailsPage - ChooseFromLibraryPopup
+			// Navigation userDetailsPage - ChooseFromLibraryPopup
 			customerProfilePage.userDetailsPage().clickEditUserImage();
 			customerProfilePage.userDetailsPage().editUserImagePopup().clickChooseFromLibrary();
-			customerProfilePage.userDetailsPage().editUserImagePopup().chooseFromLibraryPopup().navigationComponent().clickClose();
+			customerProfilePage.userDetailsPage().editUserImagePopup().chooseFromLibraryPopup().navigationComponent()
+					.clickClose();
 			customerProfilePage.userDetailsPage().verifyHeading(data.get("heading"));
-			
-			
-			}catch(Exception e) {
-				ExtentTestManager.setFailMessageInReport("Test user details view failed due to exception " + e );
-			}
+
+		} catch (Exception e) {
+			ExtentTestManager.setFailMessageInReport("Test user details view failed due to exception " + e);
 		}
+	}
+
+	// added
+	@Test
+	@Parameters({ "strParams" })
+	public void testCustomerProfileView(String strParams) {
+
+		try {
+			Map<String, String> data = Runner.getKeywordParameters(strParams);
+			tokenAccountPage.clickProfile();
+			customerProfilePage.verifyImageUserView();
+			customerProfilePage.verifyUserNameView();
+			customerProfilePage.verifyAccountIDView();
+			customerProfilePage.verifyAccountStatusView();
+			customerProfilePage.verifyImageUserView();
+
+		} catch (Exception e) {
+			ExtentTestManager.setFailMessageInReport("TestCustomerProfileView failed due to exception " + e);
+		}
+
+	}
+
+	// added
+	@Test
+	@Parameters({ "strParams" })
+	public void testClickCustomerProfile(String strParams) {
+		try {
+			Map<String, String> data = Runner.getKeywordParameters(strParams);
+			tokenAccountPage.clickProfile();
+			customerProfilePage.verifyUserNameView();
+			customerProfilePage.clickQRImage();
+
+		} catch (Exception e) {
+			ExtentTestManager.setFailMessageInReport("TestClickCustomerProfile failed due to exception " + e);
+		}
+
+	}
+
+	// added
+	@Test
+	@Parameters({ "strParams" })
+	public void testCopyTextAndShare(String strParams) {
+		try {
+			Map<String, String> data = Runner.getKeywordParameters(strParams);
+			tokenAccountPage.clickProfile();
+			customerProfilePage.clickQRImage();
+			customerProfilePage.verifyLabelMyQRCodeView();
+			customerProfilePage.verifyLabelCopyText();
+			customerProfilePage.clickShare();
+			customerProfilePage.verifyPopUp();
+
+		} catch (Exception e) {
+			ExtentTestManager.setFailMessageInReport("TestCopyTextAndShare failed due to exception " + e);
+		}
+
+	}
+
+	// added
+	@Test
+	@Parameters({ "strParams" })
+	public void testCrossIcon(String strParams) {
+		try {
+			Map<String, String> data = Runner.getKeywordParameters(strParams);
+			tokenAccountPage.clickProfile();
+			customerProfilePage.clickQRImage();
+			customerProfilePage.navigationComponent().clickClose();
+			customerProfilePage.verifyUserNameView();
+
+		} catch (Exception e) {
+			ExtentTestManager.setFailMessageInReport("TestCrossIcon failed due to exception " + e);
+		}
+
+	}
+
 }
