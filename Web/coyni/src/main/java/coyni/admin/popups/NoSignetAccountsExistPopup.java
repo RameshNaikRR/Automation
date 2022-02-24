d573b607841d498db20cb6165abbe408a6038d33
Afri1996 <afrinp@ideyalabs.com>
2022-02-24 15:48:20 +0530

    Added No signet Accounts Exist Popup

---
+++ Web/coyni/src/main/java/coyni/admin/popups/NoSignetAccountsExistPopup.java
@@ -0,0 +1,30 @@
+package coyni.admin.popups;
+
+import org.openqa.selenium.By;
+
+import coyni.uitilities.CommonFunctions;
+import ilabs.WebFramework.BrowserFunctions;
+
+public class NoSignetAccountsExistPopup extends BrowserFunctions {
+
+	private By lblHeading = By.xpath("//h1[text()='No Signet Accounts Exist']");
+	private By lblDescription = By.xpath("//p[@class='mt-12 leading-6 text-center ']");
+	private By btnAddSignet = By.xpath("//span[@class='label text-cgy3']");
+
+	public void verifyPageHeading(String expHeading) {
+		new CommonFunctions().verifyLabelText(lblHeading, "Remove signet Account Heading", expHeading);
+	}
+
+	public void verifyPageDescription(String expDescription) {
+		new CommonFunctions().verifyLabelText(lblDescription, "Add New signet Account Heading", expDescription);
+	}
+
+	public void clickSave() {
+		click(btnAddSignet, "Click on Add Signet Account");
+	}
+
+	public AddNewSignetAccountPopup addNewSignetAccountPopup() {
+		return new AddNewSignetAccountPopup();
+	}
+
+}
