54c965f46422dda47643fcdab6944255f89619a4
prajwalreddychennur <prajwalc@ideyalabs.com>
2022-03-02 13:19:20 +0530

    Removed PhoneVerification Component

--- Web/coyni/src/main/java/coyni/admin/pages/ForgotEmailNamePage.java
+++ Web/coyni/src/main/java/coyni/admin/pages/ForgotEmailNamePage.java
@@ -2,16 +2,17 @@ package coyni.admin.pages;
 
 import org.openqa.selenium.By;
 
-import coyni.admin.components.PhoneVerificationComponent;
+import coyni.admin.components.PhoneEmailVerificationComponent;
 import coyni.uitilities.CommonFunctions;
 import ilabs.WebFramework.BrowserFunctions;
+
 public class ForgotEmailNamePage extends BrowserFunctions {
 	private By lblHeading = By.cssSelector(".forgot-email__title");
 	private By lblDescription = By.cssSelector(".forgot-email__sub-title");
-	private By txtFirstName=By.cssSelector("#First-Name");
-	private By txtLastName=By.cssSelector("#Last-Name");
-	private By btnNext=By.xpath("//button[text()='Next']");
-	private By lnkBackToLogin =By.xpath("//div[text()='Back to Login']");
+	private By txtFirstName = By.cssSelector("#First-Name");
+	private By txtLastName = By.cssSelector("#Last-Name");
+	private By btnNext = By.xpath("//button[text()='Next']");
+	private By lnkBackToLogin = By.xpath("//div[text()='Back to Login']");
 
 	public void verifyPageHeading(String expHeading) {
 		new CommonFunctions().verifyLabelText(lblHeading, "expHeading", expHeading);
@@ -24,18 +25,21 @@ public class ForgotEmailNamePage extends BrowserFunctions {
 	public void fillFirstName(String firstName) {
 		enterText(txtFirstName, firstName, "firstName");
 	}
-    public void fillLastName(String lastName) {
-	enterText(txtLastName, lastName, "lastName");
-}
+
+	public void fillLastName(String lastName) {
+		enterText(txtLastName, lastName, "lastName");
+	}
+
 	public void clickNext() {
 		click(btnNext, "Next");
 	}
+
 	public void lnkBackToLogin() {
 		click(lnkBackToLogin, "BackToLogin");
 	}
-	
-	public PhoneVerificationComponent phoneVerificationComponent() {
-		return new PhoneVerificationComponent();
+
+	public PhoneEmailVerificationComponent phoneVerificationComponent() {
+		return new PhoneEmailVerificationComponent();
 	}
 
 }
