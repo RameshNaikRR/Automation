71e44bb638f597a86132559877bbbc6ee9f322dc
Afri1996 <afrinp@ideyalabs.com>
2022-03-10 12:57:51 +0530

    Added classes in API Business

---
+++ Web/coyni/src/main/java/coyni/apibusiness/pages/APIAdminProfilePage.java
@@ -0,0 +1,67 @@
+package coyni.apibusiness.pages;
+
+import org.openqa.selenium.By;
+
+import coyni.customer.components.AccountLimitsComponent;
+import coyni.customer.components.AuthyComponent;
+import coyni.customer.components.ChangePasswordComponent;
+import coyni.customer.components.CustomerMenuComponent;
+import coyni.customer.components.PaymentMethodsComponent;
+import coyni.customer.components.PreferencesComponent;
+import coyni.customer.components.ToastComponent;
+import coyni.customer.components.UserDetailsComponent;
+import coyni.uitilities.CommonFunctions;
+
+public class APIAdminProfilePage {
+	private By imageImg = By.cssSelector(" ");
+	private By lblAccountId = By.xpath("//div[contains(text(),'Account ID:')]");
+	private By lblUserName = By.xpath("//div[contains(text(),'Account ID:')]/preceding-sibling::div ");
+
+	public void verifyViewImage() {
+		new CommonFunctions().elementView(imageImg, "imageImg");
+	}
+
+	public void verifyAccountIdView() {
+		new CommonFunctions().elementView(lblAccountId, "AccountId");
+	}
+
+	public void verifyUserNameView() {
+		new CommonFunctions().elementView(lblUserName, "AccountUser");
+	}
+
+	public CustomerMenuComponent customerMenuComponent() {
+		return new CustomerMenuComponent();
+	}
+
+	public UserDetailsComponent userDetailsComponent() {
+		return new UserDetailsComponent();
+	}
+
+	public PaymentMethodsComponent paymentMethodsComponent() {
+		return new PaymentMethodsComponent();
+	}
+
+	public PreferencesComponent preferencesComponent() {
+		return new PreferencesComponent();
+	}
+
+	public ToastComponent toastComponent() {
+		return new ToastComponent();
+	}
+
+	public ChangePasswordComponent changePasswordComponent() {
+		return new ChangePasswordComponent();
+	}
+
+	public CommonFunctions commonFunctions() {
+		return new CommonFunctions();
+	}
+
+	public AuthyComponent authyComponent() {
+		return new AuthyComponent();
+	}
+
+	public AccountLimitsComponent accountLimitsComponent() {
+		return new AccountLimitsComponent();
+	}
+}
