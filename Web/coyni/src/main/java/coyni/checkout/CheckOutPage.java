0f0c7b65983db53ee13ddc11b6455789d7797de1
manikanthv <manikanthv@ideyalabs.com>
2022-09-06 15:02:14 +0530

    dfds

--- Web/coyni/src/main/java/coyni/checkout/CheckOutPage.java
+++ Web/coyni/src/main/java/coyni/checkout/CheckOutPage.java
@@ -1,7 +1,14 @@
 package coyni.checkout;
 
+import org.openqa.selenium.By;
+
 import ilabs.WebFramework.BrowserFunctions;
 
 public class CheckOutPage extends BrowserFunctions{
+	
+	private By txtDomain=By.xpath("//input[@id='domain']");
+	private By txtOrderId=By.xpath("//input[@id='order-id']");
+	private By txtPublicKey=By.xpath("//input[@id='public-key']");
+	private By txtSecretKey=By.xpath("//input[@id='secret-key']");
 
 }
