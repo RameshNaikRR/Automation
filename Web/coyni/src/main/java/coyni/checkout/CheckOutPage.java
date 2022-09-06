6498174e2f218d315dead3facaf6c1a5443c9b32
manikanthv <manikanthv@ideyalabs.com>
2022-09-06 17:07:54 +0530

    Added methods in CheckOut Test

--- Web/coyni/src/main/java/coyni/checkout/CheckOutPage.java
+++ Web/coyni/src/main/java/coyni/checkout/CheckOutPage.java
@@ -1,7 +1,9 @@
 package coyni.checkout;
 
 import org.openqa.selenium.By;
+
 import ilabs.WebFramework.BrowserFunctions;
+import ilabs.api.reporting.ExtentTestManager;
 
 public class CheckOutPage extends BrowserFunctions {
 
@@ -9,21 +11,97 @@ public class CheckOutPage extends BrowserFunctions {
 	private By txtOrderId = By.xpath("//input[@id='order-id']");
 	private By txtPublicKey = By.xpath("//input[@id='public-key']");
 	private By txtSecretKey = By.xpath("//input[@id='secret-key']");
+	private By clkSaveDetails = By.xpath("//button[text()='Save Details']");
+	private By clkSaveClearDetails = By.xpath("//button[text()='Clear Details']");
+	private By clkCheckOut = By.xpath("//button[text()='Checkout']");
+	private By lblAmount = By.xpath("//h6[text()='Price: $']");
 
-	public void fillDomain(String domain) {
-		enterText(txtDomain, domain, "Domain");
+	private By lblCountItems = By.xpath("//span[contains(text(), 'Cart')]");
+
+	private By lblTotalAmount = By.xpath("//span[contains(text(), 'Total Amount: ')]");
+
+	private By clkCardOne = By.xpath("(//button[text()='Add To Cart'])[1]");
+	private By clkCardTwo = By.xpath("(//button[text()='Add To Cart'])[2]");
+
+	public By getCard(String index) {
+		return By.xpath(String.format("(//button[text()='Add To Cart'])[%s]", index));
 	}
 
-	public void fillOrderID(String orderId) {
-		enterText(txtOrderId, orderId, "Order ID");
+	public void enterDomain(String text) {
+		enterText(txtDomain, text, "Domain");
 	}
 
-	public void fillPublicKey(String publicKey) {
-		enterText(txtPublicKey, publicKey, "Public Key");
+	public void enterOrderId(String text) {
+		enterText(txtOrderId, text, "Order ID");
 	}
 
-	public void fillSecretKey(String secretKey) {
-		enterText(txtSecretKey, secretKey, "Secret Key");
+	public void enterPublicKey(String text) {
+		enterText(txtPublicKey, text, "Public key");
+	}
+
+	public void enterSecretKey(String text) {
+		enterText(txtSecretKey, text, "Secret Key");
+	}
+
+	public void clickSaveDetails() {
+		click(clkSaveDetails, "Save Details");
+	}
+
+	public void clickClearDetails() {
+		click(clkSaveClearDetails, "Clear Details");
+	}
+
+	public void clickCheckOut() {
+		click(clkCheckOut, "CheckOut");
+	}
+
+	public void clickCardOne() {
+		click(clkCardOne, "Card One");
+	}
+
+	public void clickCardTwo() {
+		click(clkCardTwo, "Card One");
+	}
+
+	public String getItemCount() {
+		String text = getText(lblCountItems, "");
+		String replaceAll = text.replaceAll("[^0-9]", "");
+		ExtentTestManager.setInfoMessageInReport("Total Item is : " + replaceAll);
+		return replaceAll;
+	}
+
+	public String getTotalAmount() {
+		String text = getText(lblTotalAmount, "");
+		String replaceAll = text.replaceAll("[^0-9]", "");
+		ExtentTestManager.setInfoMessageInReport("Total Amont is : " + replaceAll);
+		return replaceAll;
+	}
+
+	public By getFirstCardAmount(String index) {
+		return By.xpath(String.format("(//h6[text()='Price: $'])[%s]", index));
+	}
+
+	private By lblAmountFirst = By.xpath("(//h6[text()='Price: $'])[1]");
+	private By lblAmountSecond = By.xpath("(//h6[text()='Price: $'])[2]");
+
+	public int getTotalFirstCardAmount() {
+		String text = getText(lblAmountFirst, "Amount ");
+		int replaceAll =Integer.parseInt(text.replaceAll("[^0-9]", ""));
+		ExtentTestManager.setInfoMessageInReport("First Card Amount is : " + replaceAll);
+		return replaceAll;
+	}
+
+	public int getTotalSecondCardAmount() {
+		String text = getText(lblAmountSecond, "Amount");
+		int replaceAll = Integer.parseInt(text.replaceAll("[^0-9]", ""));
+		ExtentTestManager.setInfoMessageInReport("Seccond Card Amount is : " + replaceAll);
+		return replaceAll;
+
+	}
+
+	public int sumOfAmount() {
+		int s = getTotalFirstCardAmount() + getTotalSecondCardAmount();
+		return s;
 	}
 
 }
--- Web/coyni/src/test/java/coyni/checkout/tests/CheckOutTest.java
+++ Web/coyni/src/test/java/coyni/checkout/tests/CheckOutTest.java
@@ -1,9 +1,11 @@
 package coyni.checkout.tests;
 
 import java.util.Map;
-import org.testng.annotations.BeforeMethod;
+
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import coyni.checkout.CheckOutPage;
 import ilabs.WebFramework.Runner;
 import ilabs.api.reporting.ExtentTestManager;
@@ -12,26 +14,40 @@ public class CheckOutTest {
 
 	CheckOutPage checkOutPage;
 
-	@BeforeMethod
+	@BeforeTest
 	public void init() {
-		checkOutPage = new CheckOutPage();
 
+		checkOutPage = new CheckOutPage();
 	}
 
 	@Test
 	@Parameters({ "strParams" })
-	public void testCheckOut(String strParams) {
+
+	public void tesCheckOutTransaction(String strParams) {
 		try {
 			Map<String, String> data = Runner.getKeywordParameters(strParams);
-			checkOutPage.fillDomain(data.get("domain"));
-			checkOutPage.fillOrderID(data.get("orderId"));
-			checkOutPage.fillPublicKey(data.get("publicKey"));
-			checkOutPage.fillSecretKey(data.get("secretKey"));
-			
+			checkOutPage.enterDomain(data.get("domain"));
+			checkOutPage.enterOrderId(data.get("orderId"));
+			checkOutPage.enterPublicKey(data.get("publicKey"));
+			checkOutPage.enterSecretKey(data.get("secretKey"));
+			checkOutPage.clickSaveDetails();
+			checkOutPage.clickCardOne();
+			int totalFirstCardAmount = checkOutPage.getTotalFirstCardAmount();
+			checkOutPage.clickCardTwo();
+			int totalSecondCardAmount = checkOutPage.getTotalSecondCardAmount();
+			int count = totalFirstCardAmount + totalSecondCardAmount;
+			int sumOfAmount = checkOutPage.sumOfAmount();
+			if (count == sumOfAmount) {
+				ExtentTestManager.setInfoMessageInReport("Total Amount is : " + sumOfAmount);
+				checkOutPage.clickCheckOut();
+
+			} else {
+				ExtentTestManager.setFailMessageInReport("Amount Is not matched");
+			}
 
 		} catch (Exception e) {
-			ExtentTestManager.setFailMessageInReport("Login test view failed due to exception " + e);
+			ExtentTestManager.setFailMessageInReport(" test CheckOut transaction is failed due to Exception " + e);
 		}
-	}
 
+	}
 }
