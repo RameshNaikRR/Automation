598952d5f7f68ba0629f9208466ac745aaf1bc67
manikanthv <manikanthv@ideyalabs.com>
2023-02-08 16:12:12 +0530

    Added new class for Add merchant from Admin Side

---
+++ Web/coyni/src/main/java/coyni/admin/components/AddMerchantComponent.java
@@ -0,0 +1,21 @@
+package coyni.admin.components;
+
+import org.openqa.selenium.By;
+
+import ilabs.WebFramework.BrowserFunctions;
+
+public class AddMerchantComponent extends BrowserFunctions {
+	
+	private By lblHeading=By.xpath("");
+	private By lblContent=By.xpath("");
+	private By btnMerchant=By.xpath("");
+	private By txtFirstName=By.xpath("");
+	private By txtLastName=By.xpath("");
+	private By txtEmail=By.xpath("");
+	private By txtCompanyName=By.xpath("");
+	private By txtPartnerName=By.xpath("");
+	private By btnSendInvitation=By.xpath("");
+
+	
+
+}
